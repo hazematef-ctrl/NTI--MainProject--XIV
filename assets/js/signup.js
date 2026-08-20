1afe9df214fd71114f05fcd2035b359d150e17e1
hazem atef <hazematefhassan1234@gmail.com>
2026-08-20 14:50:08 +0300

    Fix redirection URL in signup form
    Updated redirection URL after successful signup.

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -31,7 +31,7 @@ form.addEventListener("submit", function (m) {
         message.className = "success";
 
         setTimeout(function () {
-            window.location.href = "home.html";
+        window.location.href = "../../index.html";
         }, 1000);
     }
 
